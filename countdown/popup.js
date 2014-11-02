c7f43830ab6cf7e169852c8601a38654e4698f59
Nelle McDade <nelle@Nelles-MacBook-Pro.local>
2014-11-02 13:58:04 -0500

    getting closer to something
    Added a block functions - still not working properly
    Selections are not connected or working

--- countdown/popup.js
+++ countdown/popup.js
@@ -1,3 +1,16 @@
+window.onload = function(){ 
+    // your code 
+
+(function () {
+    if (window.addEventListener) {
+        window.addEventListener('DOMContentLoaded', domReady, false);
+    } else {
+        window.attachEvent('onload', domReady);
+    }
+} ());
+
+function domReady() {
+
 var refreshDisplayTimeout;
 var bgpage = chrome.extension.getBackgroundPage();
 var previousValues = [15, 30, 45, 60];
@@ -35,12 +48,12 @@ function load()
     hide("resume");
     editing = false;
     
-    if(bgpage.pauseDate)
+ if(bgpage.pauseDate)
     {
         showInline("resume");
         hide("pause");
     }
-    
+   
     // if timer off, show settings
 	if(!bgpage.alarmDate)
 	{
@@ -154,9 +167,9 @@ function isValid(amt)
 {
 	if(isNaN(amt) || (amt == null))
 		return false;
-	else if((amt < 0) || (amt > 240))
+			else if((amt < 0) || (amt > 240))
 		return false;
-	else
+			else
 		return true;
 }
 
@@ -165,7 +178,7 @@ function refreshDisplay()
     percent = bgpage.getTimeLeftPercent();
     
     if(percent < 15)
-        document.getElementById("bar").style.color = "grey";
+        document.getElementById("bar").style.color = "red";
 	document.getElementById("bar").textContent = bgpage.getTimeLeftString();
     document.getElementById("bar").style.width = percent + "%";
     
@@ -204,3 +217,77 @@ function reset()
 	show("settings");
 	hide("modify");
 }
+
+document.getElementById('button').onclick = function() {
+     console.log("blocked allowed_urls")
+ 		alert("allowed_urls blocked get to work!")
+}
+
+var blockUrls = {};
+
+blockUrls.DEFAULT_DELAY_SECONDS = 20 // *1000 for milliseconds used by js
+
+if (localStorage.blockUrls_DELAY_SECONDS) {
+    blockUrls.PAUSE = localStorage.blockUrls_DELAY_SECONDS*1000;
+} else {
+    blockUrls.PAUSE = blockUrls.DEFAULT_DELAY_SECONDS*1000; 
+}
+
+if (localStorage.blockUrls_ALLOWED_URLS) {
+    var allowed_urls = localStorage.blockUrls_ALLOWED_URLS;
+} else {
+    var allowed_urls = [
+                        "google.com", 
+                        "stackoverflow.com",
+                        "amazonaws.com", 
+                        "amazon.com", 
+            			"gmail.com",
+            			"bing.com",
+       					"chrome://extension",
+            			"8tracks.com",
+            			"youtube.com",
+                        "dropbox.com",
+                        "chrome-extension://djefgihkfilidjkgnbccipmkhhpkmcaa/popup.html",
+                        "getbootstrap.com"];
+}
+
+
+
+
+blockUrls.delay_page_load = function() {    
+    if (localStorage.blockUrls_ON_OR_OFF && localStorage.blockUrls_ON_OR_OFF != "off") {
+        var on_or_off = 1;
+    } else {
+        var on_or_off = 0;
+    }
+    on_or_off = 1;
+
+    if (on_or_off) {
+        var host = location.href;
+        var num_allowed = allowed_urls.length;
+        var allowed = 0;
+        for (i=0; i<num_allowed; i++) {
+            reg_exp = "^.*"+allowed_urls[i]+".*$";
+            if (host.match(reg_exp)) {
+                allowed = 1;
+                break;
+            }
+        }
+
+        if (!allowed) {
+            console.log("not an important page: pausing pageload for "+ (blockUrls.PAUSE/1000) +" seconds");
+            console.log(host);
+            var date = new Date();
+            var curDate;
+            do { curDate = new Date(); }
+            while ( curDate-date < blockUrls.PAUSE);
+        }
+    }
+};
+
+blockUrls.delay_page_load();
+
+
+}
+
+};
